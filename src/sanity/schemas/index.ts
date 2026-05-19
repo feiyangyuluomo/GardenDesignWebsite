ab80147edd596f37ec810635bb804abea0c2b5e1
feiyangyuluomo <feiyangyuluomo@gmail.com>
2026-05-19 22:16:20 +0800

    Remove sanity CLI/studio deps that cause npm conflicts

--- src/sanity/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-import { siteSettings } from './siteSettings'
-import { project } from './project'
-import { service } from './service'
-import { article } from './article'
-
-export const schemaTypes = [siteSettings, project, service, article]
